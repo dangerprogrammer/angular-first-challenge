1078cdac7410930f56e456326506e40eca92c91a
Danger <104269678+dangerprogrammer@users.noreply.github.com>
2024-01-31 19:01:06 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { AppHeaderComponent } from './app-header/app-header.component';
 import { LeftSidebarComponent } from './left-sidebar/left-sidebar.component';
 import { MainContentComponent } from './main-content/main-content.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
